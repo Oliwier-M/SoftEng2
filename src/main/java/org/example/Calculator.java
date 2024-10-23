f872f7a24d17208db7cb8b950e05aef96f9072ec
Oliwier Mordalski <276085@student.pwr.edu.pl>
2024-10-23 21:09:13 +0200

    (hotfix) division now handles division by 0, added main

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,6 +1,9 @@
 package src.main.java.org.example;
 
 public class Calculator {
+    public static void main(String[] args) {
+        System.out.println(division(5, 2));
+    }
 
     public static int addition(int x, int y){
         return x + y;
@@ -14,7 +17,11 @@ public class Calculator {
         return x * y;
     }
 
-    public static double division(int x, int y){
-        return x / y;
+    public static double division(double x, double y){
+        if(y==0){
+            throw new RuntimeException("Cannot divide by 0");
+        }else {
+            return x / y;
+        }
     }
 }
